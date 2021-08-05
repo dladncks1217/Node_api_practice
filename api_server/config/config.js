67f1c952cc80e3cd89d7040315cdfb36f0abdd41
dladncks1217 <dlaxodud1217@gmail.com>
2021-08-05 15:19:54 +0900

    update dotenv

--- api_server/config/config.js
+++ api_server/config/config.js
@@ -1,8 +1,9 @@
-{
+require('dotenv').config();
+module.exports = {
   "development": {
     "username": "root",
-    "password": "l9324972im",
-    "database": "node_api",
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
